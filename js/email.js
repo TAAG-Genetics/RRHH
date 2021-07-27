c5bae8aef7651ac1ba526468b2c3e14f809f1b7e
Adrian Madrid <jorgeadrianmc@gmail.com>
2021-07-27 16:47:05 -0500

    Se realizó estructura y vista de celula inicial

---
+++ js/email.js
@@ -0,0 +1,34 @@
+var formulario = document.getElementById('formulario');
+var respuesta = document.getElementById('respuesta');
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('me diste un click')
+
+    var datos = new FormData(formulario);
+
+    console.log(datos)
+    console.log(datos.get('email'))
+
+    fetch('Archivo a utilizar ruta',{
+        method: 'POST',
+        body: datos
+    })
+        .then( res => res.json())
+        .then( data => {
+            console.log(data)
+            if(data === 'error'){
+                respuesta.innerHTML = `
+                <div class="alert alert-danger" role="alert">
+                    Coloca tu email
+                </div>
+                `
+            }else{
+                respuesta.innerHTML = `
+                <div class="alert alert-primary" role="alert">
+                    ${data}
+                </div>
+                `
+            }
+        } )
+})
